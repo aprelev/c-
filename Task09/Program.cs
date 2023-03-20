e998e6fdf1b2010d059350f7b92b213c7239844a
aprelev <aprelev.barber@bk.ru>
2023-03-20 23:02:46 +0300

    Решена 9 задача

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -8,7 +8,7 @@ int number = new Random().Next(10,100);
 Console.WriteLine($"Случайное число из отрезка 10 - 99 -> {number}");
 int firstDigit = number / 10;
 int secondDigit = number % 10;
-if (firstDigit > secondDigit)
+if (firstDigit > secondDigit) //88
 Console.WriteLine($"Наибольшая цифра числа -> {firstDigit}");
 else
 Console.WriteLine($"Наибольшая цифра числа -> {secondDigit}");

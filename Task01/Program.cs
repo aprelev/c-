eb70e4db4ff1a2f06fa4e8d157eaec7edb274e70
aprelev <aprelev.barber@bk.ru>
2023-03-20 12:45:29 +0300

    Решили 1 задачу

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,14 +1,22 @@
 ﻿/*
- Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-a = 5; b = 7 -> max = 7
-a = 2 b = 10 -> max = 10
-a = -9 b = -3 -> max = -3 
+Напишите программу, которая на вход принимает два числа и проверяет, является ли первое число квадратом второго.
+a = 25, b = 5 -> да
+a = 2, b = 10 -> нет
+a = 9, b = -3 -> да
+a = -3, b = 9 -> нет
 */
-Console.Write("Введите 2  числa");
-int num1 = Convert.ToInt32(Console.ReadLine());
-int num2 = Convert.ToInt32(Console.ReadLine());
-if (num1 > num2) {
-Console.WriteLine("Первое больше");
-} else {
-    Console.WriteLine("Второе больше");
+
+Console.Write("Введите первое целое число:");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе целое число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a == b * b)
+{
+    Console.WriteLine($"Квадрат числа {b} = {a} -> Да");
+}
+else
+{
+    Console.WriteLine($"Квадрат числа {b} не равняется {a} -> Нет");
 }

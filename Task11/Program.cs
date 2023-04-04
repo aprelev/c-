1575e368cee3c515feac2902a82137d27171d0c3
aprelev <aprelev.barber@bk.ru>
2023-04-04 13:48:48 +0300

    Исправили ошибки 11 задачи

--- Task11/Program.cs
+++ Task11/Program.cs
@@ -1,4 +1,4 @@
-﻿//Напишите программу которая на выводит сдучайное трехзначное число и удаляет вторую цифру этого числа.
+﻿//Напишите программу, которая на выводит сдучайное трехзначное число и удаляет вторую цифру этого числа.
 //456 -> 46
 //782 -> 72
 //918 -> 98
@@ -13,6 +13,6 @@ return result;
 
 
 int number = new Random().Next(100,1000);
-int rsd = RemoveSecondDigit(number);
+int removeSecondDigit = RemoveSecondDigit(number);
 Console.WriteLine($"Случайное число из отрезка 100 - 999 -> {number}");
-Console.WriteLine($"После удаления второй цифры, получаем число: {rsd}");
+Console.WriteLine($"После удаления второй цифры, получаем число: {removeSecondDigit}");

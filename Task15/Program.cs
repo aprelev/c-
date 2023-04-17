bb149a4f91d86200aac2dd512f2348bfdd13acff
aprelev <aprelev.barber@bk.ru>
2023-04-17 14:46:54 +0300

    Решили 15 задачу

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,3 +5,15 @@
 1 -> нет
 */
 
+Console.WriteLine("Введите номер дня недели от 1 до 7");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number >= 1 && number <= 5)
+{
+    Console.WriteLine("Не выходной");
+}
+else if (number >= 6 && number <= 7)
+{
+    Console.WriteLine("Выходной");
+}
+else Console.WriteLine("В неделе всего семь дней");
